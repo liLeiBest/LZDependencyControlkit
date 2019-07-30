cdb245712d7e14a1a2e8c92983973d638176a20d
lilei <lilei_hapy@163.com>
2019-07-30 14:01:24 +0800

    Version 0.1.3，Bug，强引用

--- Example/LZDependencyControlkit/LZViewController.m
+++ Example/LZDependencyControlkit/LZViewController.m
@@ -7,23 +7,57 @@
 //
 
 #import "LZViewController.h"
+#import <LZDependencyControlkit/LZDependencyControlkit.h>
 
-@interface LZViewController ()
+@interface LZViewController () {
+	
+	IBOutlet LZLoadingButton *loadingView;
+}
 
 @end
 
 @implementation LZViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	[self setupUI];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+	
+	[loadingView animationFinish];
+	
+	[(LZLoadingButton *)self.navigationItem.rightBarButtonItem.customView animationFinish];
+}
+
+// MARK: - UI Action
+- (void)loadingSomething {
+	
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+// MARK: - Private
+- (void)setupUI {
+	
+	NSDictionary *attributes = self.navigationController.navigationBar.titleTextAttributes;
+	attributes = attributes?:@{NSFontAttributeName : [UIFont systemFontOfSize:14],
+							   NSForegroundColorAttributeName : [UIColor redColor],
+							   };
+	NSAttributedString *title = [[NSAttributedString alloc] initWithString:@"测试" attributes:attributes];
+	LZLoadingButton *loadingBtn = [[LZLoadingButton alloc] initWithTitle:title shapColor:[UIColor clearColor] frame:CGRectMake(0, 0, 80, 30)];
+	loadingBtn.circleColor = [attributes objectForKey:NSForegroundColorAttributeName];
+	loadingBtn.maskColor = [UIColor orangeColor];
+	loadingBtn.loadColor = [attributes objectForKey:NSForegroundColorAttributeName];
+	[loadingBtn addTarget:self action:@selector(loadingSomething)];
+	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:loadingBtn];
+	
+	loadingView.attributedTitle = title;
+	loadingView.shapeColor = [UIColor redColor];
+	loadingView.circleColor = [attributes objectForKey:NSForegroundColorAttributeName];;
+	loadingView.maskColor = [UIColor magentaColor];
+	loadingView.loadColor = [attributes objectForKey:NSForegroundColorAttributeName];;
+	[loadingView addTarget:self action:@selector(loadingSomething)];
 }
 
 @end
